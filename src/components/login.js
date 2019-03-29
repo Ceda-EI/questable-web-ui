695927b9e861e277c18725a7aaea604d5a157d48
Ceda EI <ceda_ei@protonmail.com>
2019-03-29 11:08:03 +0530

    Store token in localStorage

--- src/components/login.js
+++ src/components/login.js
@@ -3,12 +3,21 @@ import PropTypes from "prop-types";
 
 export default (player) => {
 	function Login(props) {
-		const [input, setInput] = useState("");
+		const [input, setInput] = useState(localStorage.getItem("token"));
 		return (
 			<div className="login">
 				<input type="text" placeholder="Token ID" value={input}
 					onChange={evt => setInput(evt.target.value)} />
-				<button onClick={() => player.auth(input).then(x => props.setLoggedIn(x))}>Submit</button>
+				<button
+					onClick={
+						() => player.auth(input)
+							.then(x => {
+								props.setLoggedIn(x);
+								if (x === true) {
+									localStorage.setItem("token", input);
+								}
+							})
+					}>Submit</button>
 			</div>
 		);
 	}
