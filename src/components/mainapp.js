78e217043bc8a5adb28dfcbb8b334fe0ab6685be
Ceda EI <ceda_ei@protonmail.com>
2019-03-30 00:16:33 +0530

    Add buttons to mainapp. Add css for mainapp

--- src/components/mainapp.js
+++ src/components/mainapp.js
@@ -6,9 +6,28 @@ import Stats from "./stats";
 export default (player) => {
 	function MainApp(props) {
 		const [ playerStats, setPlayerStats ] = useState(null);
+		// Possible values of display
+		// main: The main display with stats, buttons for list/add
+		const [ display ] = useState("main");
 		const token = localStorage.getItem("token");
 		if (playerStats === null)
 			player.player(token).then((res) => setPlayerStats(res));
+
+		let body;
+		if (display === "main")
+			body = (<>
+				<Stats playerStats={playerStats} />
+				<div className="stats lists nes-container with-title is-dark">
+					<p className="title">Quests</p>
+					<button className="nes-btn is-primary">Add Quests</button>
+					<button className="nes-btn is-primary">List Quests</button>
+				</div>
+				<div className="stats lists nes-container with-title is-dark">
+					<p className="title">Side Quests</p>
+					<button className="nes-btn is-primary">Add Side Quests</button>
+					<button className="nes-btn is-primary">List Side Quests</button>
+				</div>
+			</>);
 		return (
 			<div className="main">
 				<header>
@@ -20,7 +39,7 @@ export default (player) => {
 						}}>Logout</button>
 				</header>
 				<div className="body">
-					<Stats playerStats={playerStats} />
+					{body}
 				</div>
 			</div>
 		);
--- src/components/stats.js
+++ src/components/stats.js
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function Stats(props) {
 	if (props.playerStats === null) {
-		return <img src="./loader.svg"></img>;
+		return <div className="loader"><img alt="loader" src="./loader.svg"></img></div>;
 	} else {
 		const { xp, quests_completed, total_quests, side_quests_completed, total_side_quests } = props.playerStats;
 		return (
-			<div className="stats lists">
+			<div className="stats lists nes-container with-title is-dark">
+				<p className="title">Player Stats</p>
 				<ul className="nes-list is-circle">
 					<li> XP: {xp} </li>
 					<li> Quests: {quests_completed}/{total_quests} </li>
